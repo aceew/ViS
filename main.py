e504653b089f1bb4d6b4abccc1daf607eec25221
aceew <aaronwilliams@economist.com>
2019-10-27 10:08:42 +0000

    wip: committing

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import json
 from gensim.summarization.summarizer import summarize
+from google.cloud import storage
 
-def summarise(request):
-    print(request)
-    body = summarize(
-        "Video sponsor: Formidable (https://formidable.com/)\n\nFriction is a common, and necessary, part of team growth\u2014but when left unchecked, team friction is unhealthy for you, your coworkers, your company, and ultimately your end users.\n\nIn this presentation, I draw on my experiences at organizations large and small to illuminate the sources of team tension, how you can better understand and manage unexpected teammate reactions, and the best ways to give actionable feedback without escalating drama. Your coworkers, your organization, your users, and you will reap the benefits.\n\nAbout Lara Hogan\nLara Hogan is the co-founder of Wherewithall and the author of Designing for Performance and Building a Device Lab. Previously she was VP Engineering at Kickstarter and Engineering Director at Etsy. She champions performance as a part of the overall user experience, helps people get comfortable giving presentations, and believes it\u2019s important to celebrate career achievements with donuts."
-    , 0.5)
+def summarise(data, context):
+    print(context)
+    print(data.textPayload)
+    event = json.loads(data.textPayload)
+    bucket_name = event.bucket
+    file_name = event.name
+    if "speech-to-text.txt" not in file_name:
+        return
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.get_blob(file_name)
+    text = blob.download_as_string()
+
+    body = summarize(text, 0.5)
+
+    filepath_parts = file_name.split('/')
+    summary_file = "/"
+    summary_file.join(filepath_parts[0:len(filepath_parts - 2)])
+    blob = bucket.blob(summary_file)
+    blob.upload_from_string(
+        body,
+        content_type="text/plain"
+    )
 
     response = {
         "statusCode": 200,
